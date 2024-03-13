515c7a362ce31ad33423619b1ac71fc0aefdc8c5
saseungg <yeon14766@gmail.com>
2024-03-13 09:05:26 +0900

    feat: 탭바 아이콘에 공통으로 들어가는 css 클래스 추가

--- src/components/common/TabBar.tsx
+++ src/components/common/TabBar.tsx
@@ -11,20 +11,40 @@ import {
 import { BiSolidUser, BiUser } from 'react-icons/bi';
 
 const tabs = [
-  { name: '홈', icon: <AiOutlineHome />, checkedIcon: <AiFillHome /> },
-  { name: '판매내역', icon: <AiOutlineProfile />, checkedIcon: <AiFillProfile /> },
-  { name: '관심목록', icon: <AiOutlineHeart />, checkedIcon: <AiFillHeart /> },
-  { name: '채팅', icon: <AiOutlineMessage />, checkedIcon: <AiFillMessage /> },
-  { name: '내 계정', icon: <BiUser />, checkedIcon: <BiSolidUser /> },
+  {
+    name: '홈',
+    icon: <AiOutlineHome className="defaultIcon" />,
+    checkedIcon: <AiFillHome className="checkedIcon" />,
+  },
+  {
+    name: '판매내역',
+    icon: <AiOutlineProfile className="defaultIcon" />,
+    checkedIcon: <AiFillProfile className="checkedIcon" />,
+  },
+  {
+    name: '관심목록',
+    icon: <AiOutlineHeart className="defaultIcon" />,
+    checkedIcon: <AiFillHeart className="checkedIcon" />,
+  },
+  {
+    name: '채팅',
+    icon: <AiOutlineMessage className="defaultIcon" />,
+    checkedIcon: <AiFillMessage className="checkedIcon" />,
+  },
+  {
+    name: '내 계정',
+    icon: <BiUser className="defaultIcon" />,
+    checkedIcon: <BiSolidUser className="checkedIcon" />,
+  },
 ];
 
 const TabBar = () => {
   return (
     <div className="flex h-[70px] w-full ">
-      {tabs.map(({ name, icon }, index) => (
+      {tabs.map(({ name, checkedIcon }, index) => (
         <div key={index} className="flex flex-1 flex-col items-center justify-center gap-1 hover:bg-gray-100">
-          {icon}
-          <p className="text-[11px] text-gray-500">{name}</p>
+          {checkedIcon}
+          <p className="text-[11px] text-blue-500">{name}</p>
         </div>
       ))}
     </div>
